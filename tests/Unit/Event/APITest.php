2700f4bb3200770d8b569115b72ee2d8f1188e15
leocavalcante <lc@leocavalcante.com>
2020-11-13 13:46:37 -0300

    tests: event api test

---
+++ tests/Unit/Event/APITest.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace NewRelic\Test\Unit\Event;
+
+use Mockery;
+use NewRelic\Adapter\AdapterInterface;
+use NewRelic\Adapter\PostResult;
+use NewRelic\Event\API;
+
+it('send evens', function () {
+    $event_data = ['foo' => 'bar'];
+
+    $adapter = Mockery::mock(AdapterInterface::class);
+    $adapter->shouldIgnoreMissing();
+    $adapter->expects('post')
+        ->with('https://insights-collector.newrelic.com/v1/accounts/1234567890/events', [array_merge(['eventType' => 'Test'], $event_data)])
+        ->andReturn(new PostResult(202, '{"uuid": "xxxx-xxxx-xxxx-xxxx"}'));
+
+    $api = new API('1234567890', $adapter);
+    $api->send('Test', $event_data);
+
+    $response = $api->commit();
+    expect($response->getId())->toBe('xxxx-xxxx-xxxx-xxxx');
+});
