e17afc84cf79b8a4b6eb71f58c8ebd2b544b08c1
leocavalcante <lc@leocavalcante.com>
2020-11-13 15:28:23 -0300

    types: code and request id

--- src/Adapter/PostException.php
+++ src/Adapter/PostException.php
@@ -4,4 +4,16 @@ namespace NewRelic\Adapter;
 
 final class PostException extends \Exception
 {
+    private string $requestId;
+
+    public function __construct(string $message = '', string $requestId = '', \Throwable $previous = null)
+    {
+        parent::__construct($message, 0, $previous);
+        $this->requestId = $requestId;
+    }
+
+    public function getRequestId(): string
+    {
+        return $this->requestId;
+    }
 }
